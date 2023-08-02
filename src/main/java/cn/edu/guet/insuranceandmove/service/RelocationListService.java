aa31173da36138a17482b97e7fb89c232e656090
hzy <1915278756@qq.com>
2023-08-02 21:01:36 +0800

    1、修复了查询迁改的bug！

--- src/main/java/cn/edu/guet/insuranceandmove/service/RelocationListService.java
+++ src/main/java/cn/edu/guet/insuranceandmove/service/RelocationListService.java
@@ -15,13 +15,51 @@ import java.util.List;
  * @createDate 2023-07-26 17:53:27
  */
 public interface RelocationListService extends IService<RelocationList> {
+
+    /**
+     * 查询迁改清单
+     * @param relocationListQueryDTO
+     * @return
+     */
     IPage<RelocationList> searchRelocation(RelocationListQueryDTO relocationListQueryDTO);
+
+    /**
+     * 删除迁改
+     * @param ids
+     * @return
+     */
     ResponseData deleteRelocation(List<Long> ids);
+
+    /**
+     * 新增迁改
+     * @param relocationList
+     * @return
+     */
     RelocationList createRelocationList(RelocationList relocationList);
+
+    /**
+     * 查询迁改汇总统计
+     * @param year
+     * @return
+     */
     List<RelocationStatistics> selectRelocationStatisticsByYear(int year);
+
+    /**
+     * 获取迁改清单
+     * @param id
+     * @return
+     */
     ResponseData getRelocationById(Long id);
 
+    /**
+     * 导出迁改清单Excel
+     * @param idsList
+     */
     void exportRelocationListExcel(List<Integer> idsList);
 
+    /**
+     * 导出迁改汇总统计Excel
+     * @param relocationStatisticsList
+     */
     void exportRelocationStatistics(List<RelocationStatistics> relocationStatisticsList);
 }
